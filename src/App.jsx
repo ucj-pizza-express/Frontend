35b75a4ec21b632f9b84619bee59ccc8cd6324bc
Niyaah31 <saranithavam@gmail.com>
2025-05-21 21:13:52 +0530

    hero section starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Navbar from './components/navbar'
-
+import HeroSection from './components/herojection'
 
 function App() {
-    return ( 
-    <Navbar/>  
-)
+//     return ( 
+//     <Navbar/>  
+// )
+return(<HeroSection/>)
 }
 
 export default App
