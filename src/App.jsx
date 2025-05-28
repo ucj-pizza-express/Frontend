1cc358cfd4bdb2ee77a6881784ce9263eb114ddb
Niyaah31 <saranithavam@gmail.com>
2025-05-28 11:36:55 +0530

    home page setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
 import Navbar from './components/navbar'
-import HeroSection from './components/herojection'
-
+import HeroSection from './components/herosection'
+ import CustomerFavourites from "./pages/Homepage/CustomerFavourites"
+import Hero2 from './pages/Homepage/hero2'
+import AboutSection from './pages/Homepage/aboutsection'
 function App() {
-//     return ( 
-//     <Navbar/>  
-// )
-return(<HeroSection/>)
+
+return(
+    <>
+    <Navbar/>  
+<HeroSection/>
+  <CustomerFavourites/>
+   <Hero2/>
+   <AboutSection/>
+ 
+</>
+)
 }
 
 export default App
